d039089d66d935c9f78ee5048372cb21ce510b98
2182aktb <88325687+2182aktb@users.noreply.github.com>
2026-07-02 23:23:46 +0900

    Add files via upload

---
+++ keyboards/yumiyumi17/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0x01, 0x39, 0x0d, 0x01, 0x73, 0x57, 0x5d, 0x1f}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
